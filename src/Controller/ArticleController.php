bed4a2b7865209f2337b50c1c3309ed3ef28eb41
Michael Romanoff <zelcie@gmail.com>
2018-01-31 18:22:24 -0800

    add templates

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,9 +5,10 @@ namespace App\Controller;
 
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -22,10 +23,18 @@ class ArticleController
      */
     public function show($slug)
     {
-        return new Response(sprintf(
-            'Future page to show on space article: %s',
-            $slug
-        ));
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+        return $this->render('article/show.html.twig', [
+
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
+
+            ]);
 
     }
 
