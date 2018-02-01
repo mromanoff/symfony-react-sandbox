baa9793f53643751f7b1241f9bf6ff9ebd089bbf
Michael Romanoff <zelcie@gmail.com>
2018-01-31 17:54:57 -0800

    add route

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('OMG! My first page already! Wooo!');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show on space article: %s',
+            $slug
+        ));
+
+    }
+
+}
+
